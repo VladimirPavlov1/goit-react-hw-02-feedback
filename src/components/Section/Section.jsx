cb3f0efa294d3faf9a0866acfa5115558ff52fca
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-01-20 17:01:56 +0200

    fixed

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -10,6 +10,6 @@ export const Section =({title,children})=>{
 
 Section.propTypes={
     title:PropTypes.string.isRequired,
-    children:PropTypes.element,
+    children:PropTypes.node.isRequired,
 
 }
