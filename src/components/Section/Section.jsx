bdad52596aa5ff19504820dff068ea8fa903a7e1
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-01-20 13:36:36 +0200

    end feedback

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,15 @@
+import { Title } from "./Section.styled"
+import PropTypes from 'prop-types';
+
+export const Section =({title,children})=>{
+    return <section>
+        <Title>{title}</Title>
+        {children}
+    </section>
+}
+
+Section.propTypes={
+    title:PropTypes.string,
+    children:PropTypes.element,
+
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({good,neutral,bad,total,positiveFeedback})=>(
+    <ul>
+        <li><p>Good:{good}</p></li>
+        <li><p>Neutral:{neutral}</p></li>
+        <li><p>Bad:{bad}</p></li>
+        <li><p>Total:{total}</p></li>
+        <li><p>Good:{positiveFeedback}%</p></li>
+    
+    </ul>
+)
+
+Statistics.propTypes={
+    good:PropTypes.number,
+    neutral:PropTypes.number,
+    bad:PropTypes.number,
+    total:PropTypes.number,
+    positiveFeedback:PropTypes.number,
+}
