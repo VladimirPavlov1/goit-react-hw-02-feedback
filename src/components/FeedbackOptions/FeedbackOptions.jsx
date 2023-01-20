b05a4a87f396dacbd1e196dad630052daaf28a28
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-01-20 16:14:19 +0200

    change

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,21 @@
-import { List,Btn } from "./FeedbackOptions.styled"
+import { List, Btn } from './FeedbackOptions.styled';
 import PropTypes from 'prop-types';
 
-export const FeedbackOptions=({options,onLeaveFeedback})=>
-(
-   
-    <List>
-        {options.map(option=>(
-            <li key={option}><Btn type="button" onClick={()=>{onLeaveFeedback(option)}}>{option}</Btn></li>
-        ))}
-    </List>
-)
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <List>
+    {options.map(option => (
+      <li key={option}>
+        <Btn
+          type="button"
+          onClick={() => {onLeaveFeedback(option)}}>
+          {option}
+        </Btn>
+      </li>
+    ))}
+  </List>
+);
 
-FeedbackOptions.propTypes={
-        options:PropTypes.arrayOf(PropTypes.string).isRequired,  
-        onLeaveFeedback:PropTypes.func.isRequired
-}
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
